410b05ab035c4524773d39f27268234fd983f827
Bogo <bernard.bogos@gmail.com>
2023-04-22 14:24:12 +0100

    Initial commit

---
+++ Dat8014Soft.py
@@ -0,0 +1,40 @@
+# Import the required modules
+from pymodbus.server.asynchronous import StartTcpServer
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+from twisted.internet import reactor
+from threading import Thread
+from rtdSoft import updating_writer
+
+
+# Initialize your data store
+store = ModbusSlaveContext(
+    di=ModbusSequentialDataBlock(0, [17] * 100),  # 100 discrete inputs initialized to 17
+    co=ModbusSequentialDataBlock(0, [17] * 100),  # 100 coils initialized to 17
+    hr=ModbusSequentialDataBlock(0, [17] * 100),  # 100 holding registers initialized to 17
+    ir=ModbusSequentialDataBlock(0, [17] * 100),  # 100 input registers initialized to 17
+    zero_mode=True  # use zero-based addressing
+)
+
+context = ModbusServerContext(slaves=store, single=True)  # Create a server context
+
+# Initialize the server information
+identity = ModbusDeviceIdentification()  # Create an identity object
+identity.VendorName = 'Datexel'  # Set the vendor name
+identity.ProductCode = 'DAT8014'  # Set the product code
+identity.VendorUrl = 'https://www.datexel.it/en/'  # Set the vendor url
+identity.ProductName = 'ADC Datexel DAT8014 - Modbus'  # Set the product name
+identity.ModelName = 'DAT8014'  # Set the model name
+identity.MajorMinorRevision = '1.6'  # Set the revision
+
+# Run the server
+StartTcpServer(context, identity=identity, address=("127.0.0.1", 5020),
+               allow_reuse_address=True, defer_reactor_run=True)
+
+# Start a background thread to update the context every second
+thread = Thread(target=updating_writer, args=(context,))
+thread.start()
+
+# Start the reactor loop
+reactor.run()
---
+++ rtdSoft.py
@@ -0,0 +1,30 @@
+from pymodbus.payload import BinaryPayloadBuilder
+from pymodbus.constants import Endian
+import random
+import time
+
+
+# Define a callback function to update the context
+def updating_writer(context):
+    try:
+        while True:
+            random_float_1 = random.uniform(1077.9, 1136.1)  # returns a random float between 1077.9 and 1136.1
+            new_float = random.uniform(0.2, 1)  # returns a random float between 0.2 and 1.5
+            random_float_2 = random_float_1 + new_float
+
+            # Add random_float_1 and random_float_1 as a 16-bit integer to the payload_1 and payload_2
+            builder_1 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Big)
+            builder_2 = BinaryPayloadBuilder(byteorder=Endian.Big, wordorder=Endian.Big)
+            builder_1.add_16bit_float(random_float_1)
+            builder_2.add_16bit_float(random_float_2)
+            payload_1 = builder_1.to_registers()
+            payload_2 = builder_2.to_registers()
+            # Write the payload to register 0
+            # context.setValues(3, 0, payload_1)
+            context[0x00].setValues(3, 0, payload_1)
+            # Write the payload to register 1
+            # context.setValues(3, 1, payload_2)
+            context[0x00].setValues(3, 1, payload_2)
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        print("Stopping the loop.")
